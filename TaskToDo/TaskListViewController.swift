b0dd4965963f0fd1e525210bf847764f5d3ed82a
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-27 21:06:03 +0300

    Basic screen settings, working with the basic parameters of the navigation bar and adding the "add" button

--- TaskToDo/TaskListViewController.swift
+++ TaskToDo/TaskListViewController.swift
@@ -11,11 +11,36 @@ class TaskListViewController: UITableViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        view.backgroundColor = .white
+        setupNavigationBar()
     }
     
+    
+    func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //Внешний вид панели навигации
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+    }
 
+    @objc private func addNewTask() {
+        
+    }
     
 
 }
