3f6090df49728d0adc966a1ce88fbf104a1c8346
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-27 21:49:49 +0300

    I append the persistentContainer methods and the saveContext method to StorageManager, in addition, methods for saving, editing and deleting were created

--- TaskToDo/StorageManager.swift
+++ TaskToDo/StorageManager.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskToDo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    //MARK: - Public Methods
+    func fetchData(completion: (Result<[Task], Error>) -> Void) {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            let tasks = try viewContext.fetch(fetchRequest)
+            completion(.success(tasks))
+        }
+        catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    //Сохранение данных
+    func save(_ taskName: String, completion: (Task) -> Void) {
+        let task = Task(context: viewContext)
+        task.title = taskName
+        completion(task)
+        saveContext()
+    }
+    
+    //Редактирование
+    func edit(_ task: Task, newName: String) {
+        task.title = newName
+        saveContext()
+    }
+    
+    //Удаление
+    func delete(_ task: Task) {
+        viewContext.delete(task)
+        saveContext()
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
