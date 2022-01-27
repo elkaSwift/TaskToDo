792f8d9e316c0f06eb6fb39660cf8cd7e743fe0f
Elisei Mishchenko <esm.mail@icloud.com>
2022-01-27 17:21:39 +0300

    Initial Commit

---
+++ TaskToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskToDo
+//
+//  Created by Елисей Мищенко on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
